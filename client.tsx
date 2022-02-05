a5103a25c2b97c0f7bfd704d97f8d75435a752fc
jeonghye-choi <ppllhm.0@gmail.com>
2022-02-05 22:25:22 +0900

    feat: add ReactDOM render element
    #1

---
+++ client.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import SearchGame from './SearchGame';
+
+ReactDOM.render(<SearchGame />, document.querySelector('#root'));
